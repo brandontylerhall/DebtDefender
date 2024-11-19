f9dae7d373680014b32434f75b160faf6b09a19d
Brandon Hall <bran.tyler.hall@gmail.com>
2024-11-18 22:26:10 -0500

    filled in the expense menu

--- src/Menu.java
+++ src/Menu.java
@@ -30,7 +30,7 @@ public class Menu {
                         displayIncomeMenu();
                         break;
                     case 2:
-                        System.out.println("expense");
+                        displayExpenseMenu();
                         break;
                     case 3:
                         System.out.println("help");
@@ -89,7 +89,6 @@ public class Menu {
                         displayMainMenu();
                         break;
                     case 7:
-                        System.out.println("exit");
                         exit = true;
                     default:
                         System.out.println("Invalid option\n");
@@ -99,4 +98,60 @@ public class Menu {
             }
         }
     }
+
+    public void displayExpenseMenu() {
+        boolean exit = false;
+        while (!exit) {
+            try {
+                // @formatter:off
+                System.out.println("Expense menu:\n--------------------");
+                System.out.print(
+                        """
+                        1. Add an expense
+                        2. Remove an expense
+                        3. View all expenses
+                        4. Edit an expense
+                        5. Help
+                        6. Back to main menu
+                        7. Exit
+                        --------------------
+                        """
+                );
+                // @formatter:on
+
+                System.out.print("> ");
+                switch (in.nextInt()) {
+                    case 1:
+                        System.out.println("add expense");
+                        break;
+                    case 2:
+                        System.out.println("remove expense");
+                        break;
+                    case 3:
+                        System.out.println("view all expenses");
+                        break;
+                    case 4:
+                        System.out.println("edit expense");
+                        break;
+                    case 5:
+                        System.out.println("help");
+                        break;
+                    case 6:
+                        displayMainMenu();
+                        break;
+                    case 7:
+                        exit = true;
+                    default:
+                        System.out.println("Invalid option\n");
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid option. Please select a number.");
+            }
+        }
+    }
+
+    public void displayHelpMenu() {
+
+    }
+
 }
