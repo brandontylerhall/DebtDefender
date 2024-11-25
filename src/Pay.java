970b476496d7fb74c73551e632cc1bdae7db6780
brandontylerhall <bran.tyler.hall@gmail.com>
2024-11-25 12:29:39 -0500

    cleaned up code

--- src/Pay.java
+++ src/Pay.java
@@ -1,32 +1,12 @@
 import java.util.ArrayList;
 
-public abstract class Pay extends TBD{
-    private string item;
+public abstract class Pay extends TBD {
+    private String item;
     private double amount;
+    private static ArrayList<Pay> pays = new ArrayList<>();
 
-
-
-
-    private static ArrayList <Pay> pays = new ArrayList<>();
-
-    public Pay( String item , double amount){
+    public Pay(String item, double amount) {
         this.item = item;
-        this.amount = amount;
-    }
-
-    public string getItem() {
-        return item;
-    }
-
-    public void setItem(string item) {
-        this.item = item;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
         this.amount = amount;
     }
 }
