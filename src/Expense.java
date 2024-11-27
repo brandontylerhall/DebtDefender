07953193e47d83146c85ea7f208581d984914e0f
Brandon Hall <bran.tyler.hall@gmail.com>
2024-11-26 22:46:56 -0500

    Implemented abstract classes

--- src/Expense.java
+++ src/Expense.java
@@ -1,13 +1,28 @@
+import java.time.LocalDate;
+import java.util.Scanner;
 
+public class Expense extends Entry {
+    public Expense() {
+    }
 
-public class Expense extends TBD {
-    private String item;
-    private String paymentMethod;
-    private String dueDate;
-    private double amount;
+    // Since this class is inheriting fields from its parent class and has no class specific fields, we use super simply to reduce redundancy.
+    public Expense(String name, String description, String category, int recurrence, double amount, LocalDate beginDate) {
+        super(name, description, category, recurrence, amount, beginDate);
+    }
 
-    public Expense(String item, String paymentMethod, String dueDate, double amount) {
-        this.paymentMethod = paymentMethod;
-        this.dueDate = dueDate;
+    // Since we're extending Entry in this class, we HAVE to implement the abstract classes from before otherwise Java has a stroke.
+    @Override
+    public void add(Scanner in) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void edit() {
+
     }
 }
