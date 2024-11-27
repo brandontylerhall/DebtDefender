d5acb9732d01935395a0937653e30eb443115d2e
Jequavis <jequavisgriffin@gmail.com>
2024-11-26 21:22:07 -0500

    add the Tbd unfinshed

---
+++ src/TBD.java
@@ -0,0 +1,33 @@
+public class TBD {
+
+
+
+
+
+
+    public ArrayList<String> getPay(){
+
+        return pays;
+    }
+    public void removePay(String Pay){
+        System.out.println("This Payment was removed" + Pay);
+    } else {
+        System.out.print("Your Payment was not sucssesful" + Pay);
+    }
+
+    public void addPay(String Pay){
+        Pay.add(Pay);
+
+    }
+
+    public void
+
+
+
+
+
+
+
+
+
+}
