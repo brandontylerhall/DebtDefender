7d1a32c2708e3f1e9492bfad948a27af1d9c7298
brandontylerhall <bran.tyler.hall@gmail.com>
2024-12-04 16:04:53 -0500

    Completed implementation: gives DTI, top 3 income/expense categories, and top 3 entries in those categories

--- src/ReportHandler.java
+++ src/ReportHandler.java
@@ -10,14 +10,14 @@ public class ReportHandler {
 
         double totalIncome = 0.0;
         double totalExpenses = 0.0;
-        double debtRatio = 0.0;
+        double debtRatio;
 
         for (Income income : incomes) {
-            totalIncome += (income.getAmount() * income.getRecurrence());
+            totalIncome += income.getAmountPerMonth();
         }
 
         for (Expense expense : expenses) {
-            totalExpenses += (expense.getAmount() * expense.getRecurrence());
+            totalExpenses += (expense.getAmountPerPayment() * expense.getRecurrence());
         }
 
         debtRatio = (totalExpenses / totalIncome) * 100;
@@ -40,44 +40,148 @@ public class ReportHandler {
         }
 
         in.nextLine();
-        System.out.printf("\nWould you like to break this down and go deeper? [y/n]\n> ");
+        System.out.print("\nWould you like to break this down and go deeper? [y/n]\n> ");
         String ans = in.nextLine();
 
         while (!ans.equals("y") && !ans.equals("n")) {
-            System.out.printf("\nInvalid input. Enter 'y' or 'n'.\n> ");
+            System.out.print("\nInvalid input. Enter 'y' or 'n'.\n> ");
             ans = in.nextLine();
         }
 
         if (ans.equalsIgnoreCase("y")) {
-            categoryReport(incomes, expenses, in);
+            topThreeCats(incomes, expenses, in);
         }
 
     }
 
+    public static void topThreeCats(ArrayList<Income> incomes, ArrayList<Expense> expenses, Scanner in) {
+        ArrayList<Income> largestThreeIncCats = new ArrayList<>();
+        ArrayList<Expense> largestThreeExpCats = new ArrayList<>();
+        ArrayList<String> incomeCategories = new ArrayList<>();
+        ArrayList<String> expenseCategories = new ArrayList<>();
 
-    public static void categoryReport(ArrayList<Income> incomes, ArrayList<Expense> expenses, Scanner in) {
-        ArrayList<Income> largestThreeIncomeCategories = new ArrayList<>();
-        ArrayList<Expense> largestThreeExpenseCategories = new ArrayList<>();
+        String topIncCat1 = "";
+        String topIncCat2 = "";
+        String topIncCat3 = "";
 
+        String topExpCat1 = "";
+        String topExpCat2 = "";
+        String topExpCat3 = "";
 
-        System.out.print("\nWould you like to break this report down further? [y/n]\n> ");
+        double topIncCat1Amount = 0.0;
+        double topIncCat2Amount = 0.0;
+        double topIncCat3Amount = 0.0;
 
-        in.nextLine();
+        double topExpCat1Amount = 0.0;
+        double topExpCat2Amount = 0.0;
+        double topExpCat3Amount = 0.0;
+
+        for (Income income : incomes) {
+            String category = income.getCategory();
+            double amount = income.getAmountPerMonth();
+
+            if (amount > topIncCat1Amount) {
+                topIncCat3 = topIncCat2;
+                topIncCat3Amount = topIncCat2Amount;
+                topIncCat2 = topIncCat1;
+                topIncCat2Amount = topIncCat1Amount;
+                topIncCat1 = category;
+                topIncCat1Amount = amount;
+            } else if (amount > topIncCat2Amount) {
+                topIncCat3 = topIncCat2;
+                topIncCat3Amount = topIncCat2Amount;
+                topIncCat2 = category;
+                topIncCat2Amount = amount;
+            } else if (amount > topIncCat3Amount) {
+                topIncCat3 = category;
+                topIncCat3Amount = amount;
+            }
+        }
+
+        for (Income income : incomes) {
+            String category = income.getCategory();
+            if (category.equals(topIncCat1) || category.equals(topIncCat2) || category.equals(topIncCat3)) {
+                if (!incomeCategories.contains(category)) {
+                    largestThreeIncCats.add(income);
+                    incomeCategories.add(category);
+                }
+            }
+        }
+
+        for (Expense expense : expenses) {
+            String category = expense.getCategory();
+            double amount = expense.getAmountPerMonth();
+
+            if (amount > topExpCat1Amount) {
+                topExpCat3 = topExpCat2;
+                topExpCat3Amount = topExpCat2Amount;
+                topExpCat2 = topExpCat1;
+                topExpCat2Amount = topExpCat1Amount;
+                topExpCat1 = category;
+                topExpCat1Amount = amount;
+            } else if (amount > topExpCat2Amount) {
+                topExpCat3 = topExpCat2;
+                topExpCat3Amount = topExpCat2Amount;
+                topExpCat2 = category;
+                topExpCat2Amount = amount;
+            } else if (amount > topExpCat3Amount) {
+                topExpCat3 = category;
+                topExpCat3Amount = amount;
+            }
+        }
+
+        for (Expense expense : expenses) {
+            String category = expense.getCategory();
+            if (category.equals(topExpCat1) || category.equals(topExpCat2) || category.equals(topExpCat3)) {
+                if (!expenseCategories.contains(category)) {
+                    largestThreeExpCats.add(expense);
+                    expenseCategories.add(category);
+                }
+            }
+        }
+
+        largestThreeIncCats.sort((o1, o2) -> Double.compare(o2.getAmountPerMonth(), o1.getAmountPerMonth()));
+        largestThreeExpCats.sort((o1, o2) -> Double.compare(o2.getAmountPerMonth(), o1.getAmountPerMonth()));
+
+        if (incomeCategories.isEmpty()) {
+            System.out.println("\nNothing to report for income.");
+        } else if (incomeCategories.size() == 1) {
+            System.out.print("\nYour largest (and only) category of income:");
+            System.out.printf("\n%s: $%.2f\n", incomeCategories.getFirst(), topIncCat1Amount);
+        } else {
+            System.out.printf("\nYour %d largest categories of income:\n\n", incomeCategories.size());
+            for (Income income : largestThreeIncCats) {
+                System.out.printf("%s: $%.2f\n", income.getCategory(), income.getAmountPerMonth());
+            }
+        }
+
+        if (expenseCategories.isEmpty()) {
+            System.out.println("\nNothing to report for expenses.");
+        } else if (expenseCategories.size() == 1) {
+            System.out.print("\nYour largest (and only) category of expense:");
+            System.out.printf("\n%s: $%.2f\n", expenseCategories.getFirst(), topIncCat1Amount);
+        } else {
+            System.out.printf("\nYour %d largest categories of expense:\n\n", expenseCategories.size());
+            for (Expense expense : largestThreeExpCats) {
+                System.out.printf("%s: $%.2f\n", expense.getCategory(), expense.getAmountPerMonth());
+            }
+        }
+
+        System.out.print("\nWould you like to break this report down further still? [y/n]\n> ");
         String ans = in.nextLine();
 
         while (!ans.equals("y") && !ans.equals("n")) {
-            System.out.println("Invalid input. Enter 'y' or 'n'.");
+            System.out.print("\nInvalid input. Enter 'y' or 'n'.\n> ");
             ans = in.nextLine();
         }
 
         if (ans.equals("y")) {
-            topThree(incomes, expenses, in);
+            topThree(largestThreeIncCats, largestThreeExpCats);
         }
-
-
     }
 
-    public static void topThree(ArrayList<Income> incomes, ArrayList<Expense> expenses, Scanner in) {
+
+    public static void topThree(ArrayList<Income> incomes, ArrayList<Expense> expenses) {
         ArrayList<Income> largestThreeIncomes = new ArrayList<>();
         ArrayList<Expense> largestThreeExpenses = new ArrayList<>();
         double largestAmount1 = 0.0;
@@ -85,35 +189,37 @@ public class ReportHandler {
         double largestAmount3 = 0.0;
 
         for (Income income : incomes) {
-            if (income.getAmount() > largestAmount1) {
+            if (income.getAmountPerMonth() > largestAmount1) {
                 largestAmount3 = largestAmount2;
                 largestAmount2 = largestAmount1;
-                largestAmount1 = income.getAmount();
-            } else if (income.getAmount() > largestAmount2) {
+                largestAmount1 = income.getAmountPerMonth();
+            } else if (income.getAmountPerMonth() > largestAmount2) {
                 largestAmount3 = largestAmount2;
-                largestAmount2 = income.getAmount();
-            } else if (income.getAmount() > largestAmount3) {
-                largestAmount3 = income.getAmount();
+                largestAmount2 = income.getAmountPerMonth();
+            } else if (income.getAmountPerMonth() > largestAmount3) {
+                largestAmount3 = income.getAmountPerMonth();
             }
         }
 
         for (Income income : incomes) {
-            if (income.getAmount() == largestAmount1 || income.getAmount() == largestAmount2 || income.getAmount() == largestAmount3) {
+            if (income.getAmountPerMonth() == largestAmount1 || income.getAmountPerMonth() == largestAmount2 || income.getAmountPerMonth() == largestAmount3) {
                 largestThreeIncomes.add(income);
             }
         }
 
-        largestThreeIncomes.sort((o1, o2) -> Double.compare(o2.getAmount(), o1.getAmount()));
+        largestThreeIncomes.sort((o1, o2) -> Double.compare(o2.getAmountPerMonth(), o1.getAmountPerMonth()));
 
         if (largestThreeIncomes.isEmpty()) {
             System.out.println("\nThere is no income report at this time.");
         } else {
             if (largestThreeIncomes.size() == 1) {
-                System.out.print("\nYour largest incomes:");
-                Menu.textBox(largestThreeIncomes);
+                System.out.print("\nYour largest (and only) income:\n");
+                System.out.printf("%s: %.2f", largestThreeIncomes.getFirst().getName(), largestThreeIncomes.getFirst().getAmountPerMonth());
             } else {
-                System.out.printf("\nYour %d largest incomes:", largestThreeIncomes.size());
-                Menu.textBox(largestThreeIncomes);
+                System.out.printf("\nYour %d largest incomes in the %s category:\n", largestThreeIncomes.size(), largestThreeIncomes.getFirst().getCategory());
+                for (Income income : largestThreeIncomes) {
+                    System.out.printf("%s: %.2f\n", income.getName(), income.getAmountPerMonth());
+                }
             }
         }
 
@@ -122,38 +228,38 @@ public class ReportHandler {
         largestAmount3 = 0.0;
 
         for (Expense expense : expenses) {
-            if (expense.getAmount() > largestAmount1) {
+            if (expense.getAmountPerMonth() > largestAmount1) {
                 largestAmount3 = largestAmount2;
                 largestAmount2 = largestAmount1;
-                largestAmount1 = expense.getAmount();
-            } else if (expense.getAmount() > largestAmount2) {
+                largestAmount1 = expense.getAmountPerMonth();
+            } else if (expense.getAmountPerMonth() > largestAmount2) {
                 largestAmount3 = largestAmount2;
-                largestAmount2 = expense.getAmount();
-            } else if (expense.getAmount() > largestAmount3) {
-                largestAmount3 = expense.getAmount();
+                largestAmount2 = expense.getAmountPerMonth();
+            } else if (expense.getAmountPerMonth() > largestAmount3) {
+                largestAmount3 = expense.getAmountPerMonth();
             }
         }
 
         for (Expense expense : expenses) {
-            if (expense.getAmount() == largestAmount1 || expense.getAmount() == largestAmount2 || expense.getAmount() == largestAmount3) {
+            if (expense.getAmountPerMonth() == largestAmount1 || expense.getAmountPerMonth() == largestAmount2 || expense.getAmountPerMonth() == largestAmount3) {
                 largestThreeExpenses.add(expense);
             }
         }
 
-        largestThreeExpenses.sort((o1, o2) -> Double.compare(o2.getAmount(), o1.getAmount()));
+        largestThreeExpenses.sort((o1, o2) -> Double.compare(o2.getAmountPerMonth(), o1.getAmountPerMonth()));
 
         if (largestThreeExpenses.isEmpty()) {
             System.out.println("\nThere is no expense report at this time.");
         } else {
             if (largestThreeExpenses.size() == 1) {
-                System.out.print("\nYour largest expense:");
-                Menu.textBox(largestThreeExpenses);
+                System.out.print("\nYour largest (and only) expense:\n");
+                System.out.printf("%s: %.2f", largestThreeExpenses.getFirst().getName(), largestThreeExpenses.getFirst().getAmountPerMonth());
             } else {
-                System.out.printf("\nYour %d largest expenses:", largestThreeExpenses.size());
-                Menu.textBox(largestThreeExpenses);
+                System.out.printf("\nYour %d largest expenses in the %s category:\n", largestThreeExpenses.size(), largestThreeExpenses.getFirst().getCategory());
+                for (Expense expense : largestThreeExpenses) {
+                    System.out.printf("%s: %.2f\n", expense.getName(), expense.getAmountPerMonth());
+                }
             }
         }
-
-
     }
 }
